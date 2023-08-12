41ea87e6353658466102d2aa0c4d6981755ec224
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-12 21:28:04 +0100

    03. World Snooker Championship

--- 12.aug.sat23End.js
+++ 12.aug.sat23End.js
@@ -4,8 +4,130 @@
 
 // Подготовка за изпит
 
-// 🔴 06. Barcode Generator
+// 🔴 03. World Snooker Championship
+// Задача 3. Билети за снукър
+// https://judge.softuni.org/Contests/Practice/Index/1538#5
 
+function worldSnookerChampionship(input) {
+  let stage = input[0];
+  let type = input[1];
+  let countTicket = Number(input[2]);
+  let pic = input[3];
+
+  let totalPrice = 0;
+  let isNeedPic = true;
+
+  switch (stage) {
+    case "Quarter final":
+      switch (type) {
+        case "Standard":
+          totalPrice = countTicket * 55.5;
+          break;
+        case "Premium":
+          totalPrice = countTicket * 105.2;
+          break;
+        case "VIP":
+          totalPrice = countTicket * 118.9;
+          break;
+      }
+      break;
+    case "Semi final":
+      switch (type) {
+        case "Standard":
+          totalPrice = countTicket * 75.88;
+          break;
+        case "Premium":
+          totalPrice = countTicket * 125.22;
+          break;
+        case "VIP":
+          totalPrice = countTicket * 300.4;
+          break;
+      }
+      break;
+    case "Final":
+      switch (type) {
+        case "Standard":
+          totalPrice = countTicket * 110.1;
+          break;
+        case "Premium":
+          totalPrice = countTicket * 160.66;
+          break;
+        case "VIP":
+          totalPrice = countTicket * 400;
+          break;
+      }
+      break;
+  }
+  if (totalPrice > 4000) {
+    totalPrice *= 0.75;
+    isNeedPic = false;
+  } else if (totalPrice > 2500) {
+    totalPrice *= 0.9;
+  }
+
+  if (pic === "Y" && isNeedPic) {
+    totalPrice += 40 * countTicket;
+  }
+  console.log(totalPrice.toFixed(2));
+}
+worldSnookerChampionship(["Final", "Premium", "25", "Y"]);
+worldSnookerChampionship(["Semi final", "VIP", "9", "Y"]);
+worldSnookerChampionship(["Quarter final", "Standard", "11", "N"]);
+
+// //
+// 🔴 04. Renovation
+//  https://judge.softuni.org/Contests/Practice/Index/1745#5
+
+/*
+function renovation(input) {
+  let index = 0;
+  let h = Number(input[index]);
+  index++;
+  let w = Number(input[index]);
+  index++;
+  let percentage = Number(input[index]);
+  index++;
+
+  let quadraticMForOneWalls = h * w;
+  let quadraticMForAllWalls = quadraticMForOneWalls * 4;
+  let totalQuadraticM = Math.ceil(
+    quadraticMForAllWalls * (1 - percentage / 100)
+  );
+
+  let command = input[index];
+  index++;
+
+  while (command !== "Tired!") {
+    let paint = Number(command);
+
+    totalQuadraticM -= paint;
+    if (totalQuadraticM <= 0) {
+      break;
+    }
+    command = input[index];
+    index++;
+  }
+  if (totalQuadraticM > 0) {
+    console.log(`${totalQuadraticM} quadratic m left.`);
+  } else if (totalQuadraticM < 0) {
+    console.log(
+      `All walls are painted and you have ${Math.abs(
+        totalQuadraticM
+      )} l paint left!`
+    );
+  } else {
+    console.log("All walls are painted! Great job, Pesho!");
+  }
+}
+renovation(["3", "5", "10", "2", "3", "4", "Tired!"]);
+renovation(["2", "3", "25", "6", "7", "8"]);
+*/
+
+////////////////////////////
+// 🔴 06. Barcode Generator
+// https://judge.softuni.org/Contests/Practice/Index/2507#5
+
+/*
 function barcodeGenerator(input) {
   let startInterval = String(input[0]);
   let endInterval = String(input[1]);
@@ -35,3 +157,4 @@ function barcodeGenerator(input) {
   console.log(buff);
 }
 barcodeGenerator(["2345", "6789"]);
+*/
